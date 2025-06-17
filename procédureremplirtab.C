e40c30aeaa2558e943ec2d0fba55e16695631aaa
aissatouAFI <maronea865@gmail.com>
2025-06-17 12:50:58 +0000

    Create procédureremplirtab.C

---
+++ procédureremplirtab.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void remplirTabf(int tabf[N],const int N=10) {
+    int i;
+    for (int i = 0; i <N; i++) {;
+       printf("saisir le numero facture");
+         scanf("%d", &tabf[i].numero);
+          printf("saisir le montant facture");
+         scanf("%f", &tabf[i].montant );
+          printf("saisir l'objet facture");
+         scanf("%s", &tabf[i].objet );
+          printf("saisir id étudient facture");
+         scanf("%d", &tabf[i].idetudiant);
+    }
+}
