6e32629790744b01f32d507c05c20b6e0f01a49f
lefa-04 <fallpapedjiby68@gmail.com>
2025-06-17 12:39:47 +0000

    Create sommeparobjet.c

---
+++ sommeparobjet.c
@@ -0,0 +1,16 @@
+float SommeparObjet(struct facture tabf[N]){
+	int i;
+	float Si=0;
+	float Ss=0;
+	for(i=0;i<N;i++){
+		if(strcmp(tabf[i].objet,"inscription")==0){
+			Si=Si+tabf[i].montant;
+		}
+			else if(strcmp(tabf[i].objet,"scolarite")==0){
+			Ss=Ss+tabf[i].montant;
+		}
+	}
+		printf("\n	le montant total des inscriptions est :%.2f",Si);
+		printf("\n le montant total des scolarites est :%.2f",Ss);
+		return Si,Ss;
+}
